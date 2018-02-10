1f912965213790aa664d93f784e87f548cf84b2d
zhong <zhongdu332@gmail.com>
2018-02-10 19:36:47 +0800

    20180210-002

---
+++ AdminWeb/Controllers/MgrController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AdminWeb.Controllers
+{
+    public class MgrController : Controller
+    {
+        /// <summary>
+        /// 框架首页
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ AdminWeb/Controllers/UserController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AdminWeb.Controllers
+{
+    /// <summary>
+    /// 用户信息控制器
+    /// </summary>
+    public class UserController : Controller
+    {
+        /// <summary>
+        /// 用户登录
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Login()
+        {
+            if (Request.Method.ToLower().Equals("post"))
+            {
+                return RedirectToRoute(new { controller = "Home", action = "Index" });
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        /// <summary>
+        /// 退出登录
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult LoginOut()
+        {
+            //清除登录的session信息
+
+            return Redirect("Login");
+        }
+    }
+}
